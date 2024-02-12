78163d27e732ec5c29e85b6590b02d5ef4210756
fres621 <130753126+kissingboys@users.noreply.github.com>
2024-02-12 12:07:40 +0000

    it should work

--- plugins/thingie/src/index.ts
+++ plugins/thingie/src/index.ts
@@ -1,12 +1,30 @@
 import { logger } from "@vendetta";
 import Settings from "./Settings";
+import { after, instead } from "@vendetta/patcher";
+import { storage } from "@vendetta/plugin";
+import { findByStoreName } from "@vendetta/metro";
+
+let patches = [];
+
+
+const UserStore = findByStoreName('UserStore');
 
 export default {
     onLoad: () => {
-        logger.log("Hello world!");
+        patches.push(
+            after('getUser', UserStore, (_, user) => {
+                if (!storage?.userID) return;
+                if (!storage?.avatarURL) return;
+                if (user?.id != storage?.userID) return;
+                let up = instead("getAvatarSource", user, ()=>{
+                up();
+                return {uri: storage.avatarURL };
+                });
+            })
+        );
     },
     onUnload: () => {
-        logger.log("Goodbye, world.");
+        patches.map(unpatch => unpatch());
     },
     settings: Settings,
 }
